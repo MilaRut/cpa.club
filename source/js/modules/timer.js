052dcd86d69ec035747b94b250be681239cab0e5
Mila Rut <mila.balashova2312@gmail.com>
2023-01-09 23:40:00 +0300

    feat: add new block, soldout layers, fix timer

--- source/js/modules/timer.js
+++ source/js/modules/timer.js
@@ -6,7 +6,12 @@ const daysTxt = document.querySelector('.days-txt');
 const hoursTxt = document.querySelector('.hours-txt');
 const minutesTxt = document.querySelector('.minutes-txt');
 
-let date = new Date(2023, 0, 20, 0, 0);
+const day = Number(document.querySelector('#day').textContent);
+const month = Number((document.querySelector('#month').textContent) - 1);
+
+const html = document.querySelector('html');
+
+let date = new Date(2023, month, day, 0, 0);
 
 function initCounts() {
   let now = new Date();
@@ -20,49 +25,88 @@ function initCounts() {
   hoursNum.innerText = (hours < 10) ? `0${hours}` : hours;
   minutesNum.innerText = (minutes < 10) ? `0${minutes}` : minutes;
 
-  const names = {
-    days: {
+  const namesRu = {
+    daysRu: {
       1: 'день',
       2: 'дня',
       3: 'дней',
     },
-    hours: {
+    hoursRu: {
       1: 'час',
       2: 'часа',
       3: 'часов',
     },
-    minutes: {
+    minutesRu: {
       1: 'минута',
       2: 'минуты',
       3: 'минут',
     },
   };
 
+  const namesEn = {
+    daysEn: {
+      1: 'day',
+      2: 'days',
+    },
+    hoursEn: {
+      1: 'hour',
+      2: 'hours',
+    },
+    minutesEn: {
+      1: 'minute',
+      2: 'minutes',
+    },
+  };
+
   const sliceDay = String(days).slice(-1);
-  if (parseInt(sliceDay, 10) === 1 && (parseInt(days, 10) < 10 || parseInt(days, 10) > 20)) {
-    daysTxt.innerText = names['days'][1];
-  } else if ((parseInt(sliceDay, 10) === 2 || parseInt(sliceDay, 10) === 3 || parseInt(sliceDay, 10) === 4) && (parseInt(days, 10) < 10 || parseInt(days, 10) > 20)) {
-    daysTxt.innerText = names['days'][2];
+  if (html.lang === 'ru') {
+    if (parseInt(sliceDay, 10) === 1 && (parseInt(days, 10) < 10 || parseInt(days, 10) > 20)) {
+      daysTxt.innerText = namesRu['daysRu'][1];
+    } else if ((parseInt(sliceDay, 10) === 2 || parseInt(sliceDay, 10) === 3 || parseInt(sliceDay, 10) === 4) && (parseInt(days, 10) < 10 || parseInt(days, 10) > 20)) {
+      daysTxt.innerText = namesRu['daysRu'][2];
+    } else {
+      daysTxt.innerText = namesRu['daysRu'][3];
+    }
   } else {
-    daysTxt.innerText = names['days'][3];
+    if (parseInt(sliceDay, 10) === 1 && (parseInt(days, 10) < 10 || parseInt(days, 10) > 20)) {
+      daysTxt.innerText = namesEn['daysEn'][1];
+    } else {
+      daysTxt.innerText = namesEn['daysEn'][2];
+    }
   }
 
   const sliceHours = String(hours).slice(-1);
-  if (parseInt(sliceHours, 10) === 1 && (parseInt(hours, 10) < 10 || parseInt(hours, 10) > 20)) {
-    hoursTxt.innerText = names['hours'][1];
-  } else if ((parseInt(sliceHours, 10) === 2 || parseInt(sliceHours, 10) === 3 || parseInt(sliceHours, 10) === 4) && (parseInt(hours, 10) < 10 || parseInt(hours, 10) > 20)) {
-    hoursTxt.innerText = names['hours'][2];
+  if (html.lang === 'ru') {
+    if (parseInt(sliceHours, 10) === 1 && (parseInt(hours, 10) < 10 || parseInt(hours, 10) > 20)) {
+      hoursTxt.innerText = namesRu['hoursRu'][1];
+    } else if ((parseInt(sliceHours, 10) === 2 || parseInt(sliceHours, 10) === 3 || parseInt(sliceHours, 10) === 4) && (parseInt(hours, 10) < 10 || parseInt(hours, 10) > 20)) {
+      hoursTxt.innerText = namesRu['hoursRu'][2];
+    } else {
+      hoursTxt.innerText = namesRu['hoursRu'][3];
+    }
   } else {
-    hoursTxt.innerText = names['hours'][3];
+    if (parseInt(sliceHours, 10) === 1 && (parseInt(hours, 10) < 10 || parseInt(hours, 10) > 20)) {
+      hoursTxt.innerText = namesEn['hoursEn'][1];
+    } else {
+      hoursTxt.innerText = namesEn['hoursEn'][2];
+    }
   }
 
   const sliceMinutes = String(minutes).slice(-1);
-  if (parseInt(sliceMinutes, 10) === 1 && (parseInt(minutes, 10) < 10 || parseInt(minutes, 10) > 20)) {
-    minutesTxt.innerText = names['minutes'][1];
-  } else if ((parseInt(sliceMinutes, 10) === 2 || parseInt(sliceMinutes, 10) === 3 || parseInt(sliceMinutes, 10) === 4) && (parseInt(minutes, 10) < 10 || parseInt(minutes, 10) > 20)) {
-    minutesTxt.innerText = names['minutes'][2];
+  if (html.lang === 'ru') {
+    if (parseInt(sliceMinutes, 10) === 1 && (parseInt(minutes, 10) < 10 || parseInt(minutes, 10) > 20)) {
+      minutesTxt.innerText = namesRu['minutesRu'][1];
+    } else if ((parseInt(sliceMinutes, 10) === 2 || parseInt(sliceMinutes, 10) === 3 || parseInt(sliceMinutes, 10) === 4) && (parseInt(minutes, 10) < 10 || parseInt(minutes, 10) > 20)) {
+      minutesTxt.innerText = namesRu['minutesRu'][2];
+    } else {
+      minutesTxt.innerText = namesRu['minutesRu'][3];
+    }
   } else {
-    minutesTxt.innerText = names['minutes'][3];
+    if (parseInt(sliceMinutes, 10) === 1 && (parseInt(minutes, 10) < 10 || parseInt(minutes, 10) > 20)) {
+      minutesTxt.innerText = namesEn['minutesEn'][1];
+    } else {
+      minutesTxt.innerText = namesEn['minutesEn'][2];
+    }
   }
 }
 
