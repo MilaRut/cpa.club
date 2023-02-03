2a4894398a820ea614e9103b72b16f50c24a28df
Mila Rut <mila.balashova2312@gmail.com>
2023-02-03 12:26:38 +0300

    feat: add sponsor, fix function in merchant popup

--- source/js/modules/popup-loader.js
+++ source/js/modules/popup-loader.js
@@ -1,10 +1,15 @@
-const iframe = document.querySelector('iframe');
-const loader = document.querySelector('.popup-loader');
-
-loader.classList.add('active');
-
 function initPopupLoader() {
 
+  const widgetContainer = document.getElementById('widget-container');
+  const iframe = document.querySelector('iframe');
+  const loader = document.querySelector('.popup-loader');
+
+  loader.classList.add('active');
+
+  if (!widgetContainer.hasChildNodes || iframe === null) {
+    setTimeout(initPopupLoader, 100);
+    return;
+  }
   iframe.onload = function () {
     loader.classList.remove('active');
   };
